dda7fa908a4cd71c581b1f6b815b2ee5fc339fab
AudunKodehode <audunkodehode@gmail.com>
2023-12-02 15:37:17 +0100

    *

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Routes/00Home";
 import Destination from "./Routes/01Destination";
 import Crew from "./Routes/02Crew";
